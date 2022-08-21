a96410483bba0b7a497e9920bc8e494d9e2c42d1
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-08-21 22:56:12 +0200

    csv added (not working)

--- csv_add.php
+++
@@ -1,6 +0,0 @@
-<?php // not ready to use!
-require "config.php";
-$myfile = fopen("liste.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("webdictionary.txt"));
-fclose($myfile);
-?>
