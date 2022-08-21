32edd4192a8b05cdd6627b198f74c36d36b7596d
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-08-21 21:31:00 +0200

    update now multiple values at once

--- api/in_work/update.php
+++ api/in_work/update.php
@@ -4,26 +4,41 @@ require '../config.php';
 authorize("update");
 
 // get input:
-$data = getData("POST", ["table", "column", "value_to_update", "new_value"]);
+$data = getData("POST", ["table", "id", "update"]);
 $response["message"] = "";
 $response["response"] = "";
 
 // here you will be able to modify every entry in the database
 
-$modify = new modify();
-$modify->update($data["table"], $data["column"], $data["value_to_update"], $data["new_value"]);
+foreach ($data["update"] as $key => $value)
+{
+    modify::update($data["table"], $data["id"], $key, $value);
+}
 
 echo json_encode($response); // return the response
 
 class modify
 {
-    function update($table, $column, $value_to_update, $new_value)
+    static function update($table, $id, $column, $value)
     {
         global $pdo, $response;
-        $sql = "UPDATE $table SET $column = '$new_value' WHERE $column = '$value_to_update'";
+
+        $indentityColumn = self::getIndentityColumn($table);
+        
+        $sql = "UPDATE $table SET $column = '$value' WHERE $indentityColumn = '$id'";
         $sth = $pdo->prepare($sql);
         $result = $sth->execute();
         $response["message"] = "Update success";
         $response["response"] = 1;
+    }
+
+    private static function getIndentityColumn($table)
+    {
+        global $pdo;
+        $sql = "SELECT * FROM $table";
+        $sth = $pdo->prepare($sql);
+        $sth->execute();
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
+        return array_key_first($result);
     }
 }
