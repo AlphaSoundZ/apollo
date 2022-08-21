384864d5e288ca5a4fa11e129e123e2312d38895
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-08-21 21:43:13 +0200

    simple delete of row

--- api/delete.php
+++ api/delete.php
@@ -1,34 +1,26 @@
 <?php
-require '../config.php';
+require "config.php";
 
-authorize("update");
+authorize("delete");
 
-// get input:
-$data = getData("POST", ["table", "id", "update"]);
+$data = getData("POST", ["table", "id"]);
 $response["message"] = "";
 $response["response"] = "";
 
-// here you will be able to modify every entry in the database
-
-foreach ($data["update"] as $key => $value)
-{
-    modify::update($data["table"], $data["id"], $key, $value);
-}
+delete::deleteRow($data["table"], $data["id"]);
 
 echo json_encode($response); // return the response
 
-class modify
+class delete
 {
-    static function update($table, $id, $column, $value)
+    static function deleteRow($table, $id)
     {
         global $pdo, $response;
-
         $indentityColumn = self::getIndentityColumn($table);
-        
-        $sql = "UPDATE $table SET $column = '$value' WHERE $indentityColumn = '$id'";
+        $sql = "DELETE FROM $table WHERE $indentityColumn = '$id'";
         $sth = $pdo->prepare($sql);
         $result = $sth->execute();
-        $response["message"] = "Update success";
+        $response["message"] = "Delete success";
         $response["response"] = 1;
     }
 
