93acd258f0a331c2fa5dca53e99c5e406df9a31f
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-08-22 22:28:21 +0200

    Status Code added, Check if Type exists in DB

--- api/add_device.php
+++ api/add_device.php
@@ -11,29 +11,14 @@ $response = '';
 
 authorize("add_device");
 
+$data = getData("POST", ["rfid_code", "type"]);
 
-$data = getData("POST");
 
-if ($data && !empty($data["rfid_code"]) && !empty($data["type"]))
-{
-    $checkCode = addToDB::checkCode($data["rfid_code"]);
-    if ($checkCode == false)
-    {
-        $upload = addToDB::upload($data["rfid_code"], $data["type"]);
-    }
-    else
-    {
-        $upload["response"] = 0;
-        $upload["message"] = "device already exists";
-    }
+addToDB::checkCode($data["rfid_code"], $data["type"]);
+$upload = addToDB::upload($data["rfid_code"], $data["type"]);
 
-}
-else
-{
-    $upload["response"] = 2;
-    $upload["message"] = "wrong data input";
-}
-echo json_encode($upload);
+
+
 
 
 class addToDB
@@ -43,30 +28,37 @@ class addToDB
         global $pdo;
         $sql = "INSERT INTO devices (device_id, device_type, device_uid, device_lend_user_id) VALUES (NULL, :device_type, :device_uid, '0'  )";
         $stmt= $pdo->prepare($sql);
-        $status = $stmt->execute(["device_type" => $device_type, "device_uid" => $device_uid]);
-        if ($status)
-        {
-            return ["response" => "1", "message" => "success"];
-        }
-        else
-        {
-            return ["response" => "3", "message" => "sql error"];
-        }
+        $stmt->execute(["device_type" => $device_type, "device_uid" => $device_uid]);
+        
+        http_response_code(200);
+        echo json_encode(["response" => "200", "message" => "success"]);
+        return true;
     }
-    static public function checkCode($device_uid)
+    static public function checkCode($device_uid, $type)
     {
         global $pdo;
+        // Check type
+        $sql = "SELECT device_type_id FROM property_device_type WHERE device_type_id = :type";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute(["type" => $type]);
+        if (!$stmt->fetch())
+            throw new Exception("Type does not exist", 400);
+        
+        // Check if device is already in database
         $sql = "SELECT * FROM devices WHERE device_uid = :device_uid";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(["device_uid" => $device_uid]);
         $result = $stmt->fetch();
         if ($result)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+            throw new Exception("Device already exists", 400);
+        return true;
     }
 }
+
+set_exception_handler(function ($e) {
+	$response["response"] = $e->getCode();
+	$response["message"] = $e->getMessage();
+	echo json_encode($response);
+	http_response_code($e->getCode());
+	die;
+} );
