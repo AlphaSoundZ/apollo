0559ae566a3e76e2f817a7bfa33fb8b0feecfd30
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-08-22 22:10:30 +0200

    Status Codes added

--- api/check_token.php
+++ api/check_token.php
@@ -3,6 +3,7 @@ require 'config.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use FFI\Exception;
 
 if (isset($_SERVER["HTTP_AUTHORIZATION"])) {
 $given_token = $_SERVER["HTTP_AUTHORIZATION"];
@@ -12,12 +13,14 @@ $jwt = explode(" ", $given_token)[1];
 try {
     $decoded = JWT::decode($jwt, new Key($jwt_key, 'HS256'));
     $decoded = (array) $decoded;
-    $response["response"] = 0;
+    http_response_code(200);
+    $response["response"] = 200;
     $response["message"] = "Authorized";
     $response["permissions"] = array_values((array) $decoded["permissions"]);;
     echo json_encode($response);
-} catch (\Throwable) {
-    $response["response"] = 88;
+} catch (Exception $e) {
+    http_response_code(401);
+    $response["response"] = 401;
     $response["message"] = "401 Unauthorized";
     echo json_encode($response);
     die;
