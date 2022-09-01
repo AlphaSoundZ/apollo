745cea0718c83e14eedd1b9a306bf590c2bd5690
Jan Jacob <jan.jacob.holst@gmail.com>
2022-09-01 17:21:30 +0200

    little change

--- api/v2/check_token.php
+++ api/v2/check_token.php
@@ -6,4 +6,5 @@ $given_token = $_SERVER["HTTP_AUTHORIZATION"];
 $jwt = explode(" ", $given_token)[1];
 
 $permissions["permissions"] = Token::validateToken($jwt, $_SERVER["JWT_KEY"]);
+
 Response::success(Response::SUCCESS . ": Token ist valide", "SUCCESS", $permissions);
