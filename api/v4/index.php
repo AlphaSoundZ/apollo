e95227d49ef9c722a69facbebe12929c1239a798
AlphaSoundZ <jan.jacob.holst@gmail.com>
2022-11-13 21:01:08 +0100

    ✨ feat: GET /token/validate
    validate a bearer token via authorization header
    
    #4

--- api/v4/index.php
+++ api/v4/index.php
@@ -312,6 +312,15 @@ $router->get('/token(/\d+)?', function ($id = null) {
     Response::success($response["message"], null, ["data" => $response["data"]]);
 });
 
+$rounter->get('/token/validate', function () {
+    $given_token = $_SERVER["HTTP_AUTHORIZATION"];
+    $jwt = explode(" ", $given_token)[1];
+
+    $permissions["permissions"] = Token::validateToken($jwt, $_ENV["JWT_KEY"]);
+
+    Response::success(Response::SUCCESS . ": Token ist valide", "SUCCESS", $permissions);
+});
+
 $router->get('/token/permission(/\d+)?', function ($id = null) {
     require 'classes/search_class.php';
     authorize("search");
