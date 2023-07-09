2e0050a1298cce16f33bbbdde19396fad6b0b15f
Jan Jacob Holst <85343235+AlphaSoundZ@users.noreply.github.com>
2023-07-09 13:36:49 +0200

    Revert "🎉 init: v3 branch"

---
+++ api/v5/classes/delete.class.php
@@ -0,0 +1,159 @@
+<?php
+require_once 'config.php';
+class DataDelete
+{
+    static function deleteRow($table, $id) // delete row by id in table
+    {
+        global $pdo;
+        $identityColumn = self::getIdentityColumn($table);
+
+        $sql = "SELECT * FROM $table WHERE $identityColumn = '$id'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row == false)
+            Response::error(Response::ID_NOT_FOUND, ["id"]);
+        
+        $sql = "DELETE FROM $table WHERE $identityColumn = '$id'";
+        $sth = $pdo->prepare($sql);
+        $result = $sth->execute();
+    }
+
+    static function delete($table, $id, $not_found_errorhandling = Response::ID_NOT_FOUND, $foreign_key_errorhandling = Response::FOREIGN_KEY_ERROR) 
+    {
+        global $pdo;
+        $identityColumn = self::getIdentityColumn($table);
+
+        $sql = "SELECT * FROM $table WHERE $identityColumn = '$id'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row == false) // check if id exists
+            Response::error($not_found_errorhandling, ["id"]);
+        
+        try {
+            $sql = "DELETE FROM $table WHERE $identityColumn = :id";
+            $sth = $pdo->prepare($sql);
+            $result = $sth->execute(["id" => $id]);
+            
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1451) // check for constraint error
+                Response::error($foreign_key_errorhandling, ["id"]);
+            else
+                throw $e;
+        }
+    }
+
+    static function deleteToken($id, $token)
+    {
+        global $pdo;
+        $identityColumn = self::getIdentityColumn("token");
+        // check if id is valid
+
+        $sql = "SELECT * FROM token WHERE $identityColumn = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(["id" => $id]);
+        $row = $stmt->fetch();
+
+        if (!$row)
+            Response::error(Response::TOKEN_NOT_FOUND, ["id"]);
+        else if ($id == $token['sub'])
+            Response::error(Response::DELETE_OWN_TOKEN_NOT_ALLOWED, ["id"]);
+        
+        try {
+            // delete linked permissions
+            $sql = "DELETE FROM token_link_permissions WHERE link_token_id = :id";
+            $sth = $pdo->prepare($sql);
+            $result = $sth->execute(["id" => $id]);
+
+            // delete token itself
+            $sql = "DELETE FROM token WHERE $identityColumn = :id";
+            $sth = $pdo->prepare($sql);
+            $result = $sth->execute(["id" => $id]);
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1451) // foreign key error (token_link_permissions)
+                Response::error(Response::FOREIGN_KEY_ERROR, ["id"]);
+            else // other error
+                throw $e;
+        }
+    }
+
+    static function reset($table, $reset_id, $condition = null)
+    {
+        global $pdo;
+        $sql = "SELECT COUNT(1) FROM $table";
+        $sql .= ($condition) ? " WHERE $condition" : "";
+        $sth = $pdo->query($sql);
+        $countCondition = $sth->fetchAll();
+
+        $sql = "SELECT COUNT(1) FROM $table";
+        $sth = $pdo->query($sql);
+        $countAll = $sth->fetchAll();
+
+        $sql = ($reset_id && !$condition) ? "TRUNCATE TABLE $table" : "DELETE FROM $table";
+        $sql .= ($condition) ? " WHERE $condition" : "";
+
+        if ($countCondition[0]["COUNT(1)"] == $countAll[0]["COUNT(1)"] && $reset_id)
+            $sql = "TRUNCATE TABLE $table";
+        
+        $sth = $pdo->prepare($sql);
+        $sth->execute();
+        return $countCondition[0]["COUNT(1)"];
+    }
+
+    static function clearEvent() // delete active events
+    {
+        global $pdo;
+
+        // set device_lend_user_id to 0 for all devices that are currently lent
+        $sql = "UPDATE devices SET device_lend_user_id = 0 WHERE device_lend_user_id != 0";
+        $sth = $pdo->prepare($sql);
+        $sth->execute();
+
+        // delete all events that are currently active
+        $sql = "DELETE FROM event WHERE event_end IS NULL";
+        $sth = $pdo->prepare($sql);
+        $sth->execute();
+
+        // return number of deleted rows
+        return $sth->rowCount();
+    }
+
+    static function clearUserEvent($user_id) // delete active events of user
+    {
+        global $pdo;
+
+        // check for user
+        $sql = "SELECT * FROM user WHERE user_id = :id";
+        $sth = $pdo->prepare($sql);
+        $sth->execute(["id" => $user_id]);
+
+        if (!$sth->fetch())
+            Response::error(Response::USER_NOT_FOUND, ["id"]);
+
+        // set device_lend_user_id to 0 for all devices that are currently lent
+        $sql = "UPDATE devices SET device_lend_user_id = 0 WHERE device_lend_user_id = :id";
+        $sth = $pdo->prepare($sql);
+        $sth->execute(["id" => $user_id]);
+
+        // delete all events that are currently active of user
+        $sql = "DELETE FROM event WHERE event_end IS NULL AND event_user_id = :id";
+        $sth = $pdo->prepare($sql);
+        $sth->execute(["id" => $user_id]);
+
+        // return number of deleted rows
+        return $sth->rowCount();
+    }
+
+    private static function getIdentityColumn($table)
+    {
+        global $pdo;
+        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = :table_name AND COLUMN_KEY = 'PRI'";
+        $sth = $pdo->prepare($sql);
+        $sth->execute(["table_name" => $table]);
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
+        if (!$result)
+            Response::error(Response::INTERNAL_SERVER_ERROR);
+        return $result["COLUMN_NAME"];
+    }
+}
---
+++ api/v5/paused/prebook.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'config.php';
+authorize("prebook");
+$data = getData("POST", ["user_id", "amount", "begin", "end"]);
+
+prebook($data["user_id"], $data["amount"], $data["begin"], $data["end"]);
+function prebook($user_id, $amount, $prebook_begin, $prebook_end)
+{
+    $date = date("H:i:s", strtotime($prebook_begin));
+    global $pdo;
+    
+    $sql = "SELECT * FROM devices";
+    $sth = $pdo->prepare($sql);
+    $sth->execute();
+    $highest_amount = count($sth->fetchAll());
+    
+    $date = date("Y-m-d", strtotime($prebook_begin));
+    
+    // is empty: WHYYYY
+    $sql = "SELECT * FROM prebook WHERE DATE(prebook_begin) = :date";
+    $sth = $pdo->prepare($sql);
+    $sth->execute(["date" => $date]);
+    $result = $sth->fetchAll();
+    
+    foreach ($result as $key => $value) {
+        // if there are overlapping prebook
+        if (date("H:i:s", strtotime($prebook_begin)) < date("H:i:s", strtotime($value["prebook_end"])) && date("H:i:s", strtotime($value["prebook_begin"])) < date("H:i:s", strtotime($prebook_end)))
+        {
+            $highest_amount -= $value["amount"];
+        }
+    }
+
+    if ($highest_amount >= $amount)
+    {
+
+        // check for user
+        $sql = "SELECT * FROM user WHERE user_id = :user_id";
+        $sth = $pdo->prepare($sql);
+        $sth->execute(["user_id" => $user_id]);
+        $user_exists = $sth->fetch();
+
+        if ($user_exists)
+        {
+            // prebook now
+            echo "prebook now! ($highest_amount avaiable)";
+            $sql = "INSERT INTO `prebook` (`prebook_id`, `prebook_user_id`, `prebook_amount`, `prebook_begin`, `prebook_end`) VALUES (NULL, :user_id, :amount, :begin, :end)";
+            $sth = $pdo->prepare($sql);
+            $sth->execute(["user_id" => $user_id, "amount" => $amount, "begin" => $prebook_begin, "end" => $prebook_end]);
+
+        }
+        else
+        {
+            $response["message"] = "user doesn't exist";
+            $response["response"] = "1";
+        }
+        
+    }
+    else
+    {
+        // not enough devices avaiable
+    }
+}
+
+/**
+ * - berechne wie viele Geräte in dem Zeitraum frei sind
+ * - sind genug Geräte frei?
+ * - setze Zeitraum
+ * - 
+ * 
+ * Bei der normalen Ausleihe:
+ * - Schaue ob man noch Geräte ausleihen kann, wenn man nicht vorgebucht hat. Wenn für den Tag welche zu einer Späteren Zeit vorgebucht sind, muss man diese spätestens 2 Stunden vorher ausleihen.
+ * - Wenn man vorgebucht hat, muss bei Ausleihen erst diese Zahl heruntergehen. Wenn sie bei 0 ist, kann man weitere Ausleihen, diese aber nur mit dem nichtvorgebuchten Verfahren.
+ */
