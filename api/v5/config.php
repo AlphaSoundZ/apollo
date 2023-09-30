a779ad7b88cd841679507b7d41eee4d381432800
Jan Jacob Holst <85343235+AlphaSoundZ@users.noreply.github.com>
2023-09-30 11:05:38 +0200

    Update config.php: PATCH added

--- api/v5/config.php
+++ api/v5/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Methods: DELETE, POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: DELETE, POST, GET, PATCH, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 require_once 'classes/exception_handler.class.php';
